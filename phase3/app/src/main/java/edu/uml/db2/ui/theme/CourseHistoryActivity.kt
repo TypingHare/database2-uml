c6fa946159cac3941adaf73877a4a729227022fe
Victor Ruest <victor_ruest@student.uml.edu>
2025-04-21 14:44:54 -0400

    resolved conflicts

---
+++ phase3/app/src/main/java/edu/uml/db2/ui/theme/CourseHistoryActivity.kt
@@ -0,0 +1,15 @@
+package edu.uml.db2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+
+class CourseHistoryActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
+        setContent {  }
+    }
+}
